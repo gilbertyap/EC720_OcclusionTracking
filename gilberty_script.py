533d9574f6e397a506f443112a70f8cd3fedf97b
Gilbert Yap <gilbert.yap16@gmail.com>
2021-11-04 11:58:37 -0400

    Gets Kalman filter working in script, renames some files

--- gilberty_script.py
+++ gilberty_script.py
@@ -13,7 +13,7 @@ import cv2
 
 import darknet
 import darknet_images
-import KalmanFilter from kalman-filter
+from kalmanfilter import KalmanFilter
 
 
 def main():
@@ -21,37 +21,31 @@ def main():
     # TODO - What is the frame rate of the video?
     frame_rate = 1.0/30
 
-    # TODO - Set up the Kalman filter dynamics, etc.
-    # Object dynamics
-    # Want to track x pos, y pos, x velocity, y velocity
-    F = np.array([[1, frame_rate, 0], [0, 1, frame_rate], [0, 0, 1]])
-    # Observation model
     # TODO - For now, only interested in object center position, but will move
     # tracking object velocity for identity after
-    H = np.array([1, 0, 0]).reshape(1, 3)
-    #  Covariance of the process noise
-    Q = np.array([[0.05, 0.05, 0.0], [0.05, 0.05, 0.0], [0.0, 0.0, 0.0]])
-    # Covariance of the observation noise
-    R = np.array([0.5]).reshape(1, 1)
 
-    x = np.linspace(-10, 10, 100)
-    measurements = - (x**2 + 2*x - 2)  + np.random.normal(0, 2, 100)
+    # Object dynamics
+    # Using parameters from Heimbach et al.
+    F = np.array([[1, 0, frame_rate, 0], [0, 1, 0, frame_rate], [0, 0, frame_rate, 0], [0, 0, 0, frame_rate]])
+
+    # Observation
+    H = np.array([[1, 0, 0, 0],[0, 1, 0, 0]])
+
+    #  Covariance of the process noise
+    Q = np.array([[1, 0, 6, 0], [0, 1, 0, 6], [6, 0, 2, 0], [0, 6, 0, 2]])
+
+    # Covariance of the observation noise
+    noise_cov = 0.5
+    R = np.multiply(noise_cov,np.eye(2))
 
     kf = KalmanFilter(F = F, H = H, Q = Q, R = R)
-
-    # TODO - Currently the prediction is the x-y coordinate, but in may want
-    # x and y velocity as well
-    predictions = np.array([1,2])
 
     # Randomizes the bounding box color or something?
     random.seed(3)
 
     # TODO - Remove this hard coding
-    csv_file = open('c1_seq1.csv')
+    files = darknet_images.load_images('c1_seq1.txt')
     conf_thresh=0.5
-
-    reader = csv.reader(csv_file, delimiter=',')
-    batch_size=len(reader)
 
     # Using the pre-trained weights and cfg from GitHub. Trained on MS COCO
     network, class_names, class_colors = darknet.load_network(
@@ -64,37 +58,43 @@ def main():
     # TODO - This assumes that there is only one person in the image
     # Go through every image and get the bounding box coordinates of the person
     prev_center = -1*np.ones([1,2])
+    new_center = -1*np.ones([1,2])
+    bbox_width = 0
+    bbox_height = 0
     first_detection_found = False
-    for file in reader:
+    for file in files:
         image = cv2.imread(file)
         image, detections = darknet_images.image_detection(file, network, class_names,
-                                               class_colors, batch_size=batch_size,
-                                               thresh=conf_thresh)
+                                               class_colors,thresh=conf_thresh)
 
-       if not first_detection_found:
+        if not first_detection_found:
            # "detections" has three fields: label, confidence, bbox
            # Only want detections that are "person"
            for label, confidence, bbox in detections:
                if label == 'person':
                    # Use the bounding box coordinates center
-                   xmin, ymin, xmax, ymax = bbox2points(bbox)
-                   new_center = np.array([(ymax-ymin)/2, (xmax-xmin)/2)])
+                   xmin, ymin, xmax, ymax = darknet.bbox2points(bbox)
+                   new_center = np.array([(ymax-ymin)/2, (xmax-xmin)/2])
+                   bbox_width = xmax - xmin
+                   bbox_height = ymax - ymin
+                   darknet.draw_boxes(detections,image,class_colors)
 
-                   if np.array_equal(prev_center, -1*np.ones([1,2])):
-                       # On the very first frame, simply update the center to a valid position
-                       prev_center = new_center
-                       first_detection = True
-                       continue;
-                   else:
-                       # For every other frame, update Kalman filter
-                       predictions.append(np.dot(H,  kf.predict())[0])
-                       # TODO - Make sure that new center is correct
-                       kf.update(new_center)
+                   # cv2.rectangle(image, (xmin, ymin), (xmax, ymax), class_colors['person'], 1)
+                   # cv2.putText(image, "{} [{:.2f}]".format('person', float(confidence)),
+                #               (xmin, ymin - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                #               class_colors[label], 2)
+                   # cv2.imshow('image',image)
+                   # cv2.waitKey(0)
+                   # cv2.destroyAllWindows()
+
+                   # Makes the assumption that the prev_center has not been set
+                   prev_center = new_center
+                   first_detection_found = True
 
                    # Only care about the first detection of person
                    break
 
-       else:
+        else:
            # If the person has already been identified once,
            # either update the Kalman filter with the measurement or make a
            # prediction on their position if the person is not detected
@@ -103,20 +103,38 @@ def main():
                if label == 'person':
                    person_found = True
 
-                   xmin, ymin, xmax, ymax = bbox2points(bbox)
-                   new_center = np.array([(ymax-ymin)/2, (xmax-xmin)/2)])
-                   predictions.append(np.dot(H,  kf.predict())[0])
+                   xmin, ymin, xmax, ymax = darknet.bbox2points(bbox)
+                   new_center = np.array([(ymax-ymin)/2, (xmax-xmin)/2])
+                   prediction = np.dot(H,  kf.predict())[0]
+                   print(new_center)
+                   print(prediction)
 
-                   # TODO - Make sure that new center is correct
+                   # cv2.rectangle(image, (xmin, ymin), (xmax, ymax), class_colors['person'], 1)
+                   # cv2.putText(image, "{} [{:.2f}]".format('person', float(confidence)),
+                #               (xmin, ymin - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                #               class_colors[label], 2)
+                   # cv2.imshow('image',image)
+                   # cv2.waitKey(0)
+                   # cv2.destroyAllWindows()
+
                    kf.update(new_center)
                    prev_center = new_center
 
                    # Only care about the first detection of person
                    break
 
+               # If no person is found in the frame
+               else:
+                   # TODO - I'm not sure that feeding the prediction into the update is correct but *shrug*
+                   prediction = np.dot(H,  kf.predict())[0]
+                   kf.update(prediction)
+                   print('Predicted center below')
+                   print(prediction)
+                   print('done with prediction')
+
            # Use prediction
            # TODO - how many number of frames is sustainable to keep using prediction?
-           if not person_found:
+           # if not person_found:
 
 
 if __name__ == "__main__":
