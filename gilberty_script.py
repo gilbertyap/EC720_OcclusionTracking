3e48fcf91ed589a91a2094a566f303a436e87ab5
Gilbert Yap <gilbert.yap16@gmail.com>
2021-11-05 12:13:36 -0400

    Adds drawing of prediction bounding box
    Still has an issue where the prediction for occlusion does not update the kalman filter

--- gilberty_script.py
+++ gilberty_script.py
@@ -19,7 +19,7 @@ from kalmanfilter import KalmanFilter
 def main():
     # Set up the Kalman filter object
     # TODO - What is the frame rate of the video?
-    frame_rate = 1.0/30
+    frame_rate = 1/30
 
     # TODO - For now, only interested in object center position, but will move
     # tracking object velocity for identity after
@@ -29,14 +29,15 @@ def main():
     F = np.array([[1, 0, frame_rate, 0], [0, 1, 0, frame_rate], [0, 0, frame_rate, 0], [0, 0, 0, frame_rate]])
 
     # Observation
-    H = np.array([[1, 0, 0, 0],[0, 1, 0, 0]])
+    H = np.array([[1, 0, 0, 0], [0, 1, 0, 0]])
 
     #  Covariance of the process noise
+    # TODO - better understand this
     Q = np.array([[1, 0, 6, 0], [0, 1, 0, 6], [6, 0, 2, 0], [0, 6, 0, 2]])
 
     # Covariance of the observation noise
     noise_cov = 0.5
-    R = np.multiply(noise_cov,np.eye(2))
+    R = np.multiply(noise_cov, np.eye(2))
 
     kf = KalmanFilter(F = F, H = H, Q = Q, R = R)
 
@@ -51,93 +52,89 @@ def main():
     network, class_names, class_colors = darknet.load_network(
         './/cfg//yolov4.cfg',
         './/cfg//coco.data',
-        './/yolov4.weights',
-        batch_size=batch_size
+        './/yolov4.weights'
     )
+
+    # Add a prediction window to the classes, yellow bounding box
+    class_colors['prediction']= (255,255,0)
 
     # TODO - This assumes that there is only one person in the image
     # Go through every image and get the bounding box coordinates of the person
     prev_center = -1*np.ones([1,2])
     new_center = -1*np.ones([1,2])
-    bbox_width = 0
-    bbox_height = 0
+    bbox_left = 0
+    bbox_right= 0
+    bbox_top= 0
+    bbox_bottom = 0
     first_detection_found = False
     for file in files:
-        image = cv2.imread(file)
+        # TODO - Is there way to non-person detections?
         image, detections = darknet_images.image_detection(file, network, class_names,
-                                               class_colors,thresh=conf_thresh)
-
+                                                            class_colors,thresh=conf_thresh)
         if not first_detection_found:
-           # "detections" has three fields: label, confidence, bbox
-           # Only want detections that are "person"
-           for label, confidence, bbox in detections:
-               if label == 'person':
-                   # Use the bounding box coordinates center
-                   xmin, ymin, xmax, ymax = darknet.bbox2points(bbox)
-                   new_center = np.array([(ymax-ymin)/2, (xmax-xmin)/2])
-                   bbox_width = xmax - xmin
-                   bbox_height = ymax - ymin
-                   darknet.draw_boxes(detections,image,class_colors)
+            img_size = image.shape
+            # Only want detections that are "person"
+            for label, confidence, bbox in detections:
+                if label == 'person':
+                    bbox_left, bbox_top, bbox_right, bbox_bottom = darknet.bbox2points(bbox)
+                    new_center = np.round(np.array([bbox_top+(bbox_bottom-bbox_top)/2, bbox_left+(bbox_right-bbox_left)/2]))
+                    kf.update(new_center)
 
-                   # cv2.rectangle(image, (xmin, ymin), (xmax, ymax), class_colors['person'], 1)
-                   # cv2.putText(image, "{} [{:.2f}]".format('person', float(confidence)),
-                #               (xmin, ymin - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                #               class_colors[label], 2)
-                   # cv2.imshow('image',image)
-                   # cv2.waitKey(0)
-                   # cv2.destroyAllWindows()
+                    # Show the initial frame
+                    # cv2.imshow('image',image)
+                    # cv2.waitKey(0)
+                    # cv2.destroyAllWindows()
 
-                   # Makes the assumption that the prev_center has not been set
-                   prev_center = new_center
-                   first_detection_found = True
+                    # Makes the assumption that the prev_center has not been set
+                    prev_center = new_center
+                    first_detection_found = True
 
-                   # Only care about the first detection of person
-                   break
+                    # Only care about the first detection of person
+                    break
 
         else:
-           # If the person has already been identified once,
-           # either update the Kalman filter with the measurement or make a
-           # prediction on their position if the person is not detected
-           person_found = False
-           for label, confidence, bbox in detections:
-               if label == 'person':
-                   person_found = True
+            # If the person has already been identified once,
+            # Draw both the true bounding box and the predicted bounding box
+            prediction = np.round(np.dot(H,  kf.predict())[0])
 
-                   xmin, ymin, xmax, ymax = darknet.bbox2points(bbox)
-                   new_center = np.array([(ymax-ymin)/2, (xmax-xmin)/2])
-                   prediction = np.dot(H,  kf.predict())[0]
-                   print(new_center)
-                   print(prediction)
+            person_found = False
+            for label, confidence, bbox in detections:
+                if label == 'person':
+                    person_found = True
+                    bbox_left, bbox_top, bbox_right, bbox_bottom = darknet.bbox2points(bbox)
+                    new_center = np.round(np.array([bbox_top+(bbox_bottom-bbox_top)/2, bbox_left+(bbox_right-bbox_left)/2]))
+                    kf.update(new_center)
+                    prev_center = new_center
 
-                   # cv2.rectangle(image, (xmin, ymin), (xmax, ymax), class_colors['person'], 1)
-                   # cv2.putText(image, "{} [{:.2f}]".format('person', float(confidence)),
-                #               (xmin, ymin - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                #               class_colors[label], 2)
-                   # cv2.imshow('image',image)
-                   # cv2.waitKey(0)
-                   # cv2.destroyAllWindows()
+                    # Only care about the first detection of person
+                    break
 
-                   kf.update(new_center)
-                   prev_center = new_center
+            # Predicted bounding box
+            bbox_half_width = bbox_right - prev_center[1]
+            bbox_half_height =  bbox_bottom - prev_center[0]
 
-                   # Only care about the first detection of person
-                   break
+            # Make the predicted bounding box
+            pred_bbox_left = int(np.amax([0, prediction[1]-bbox_half_width]))
+            pred_bbox_right= int(np.amin([img_size[1], prediction[1]+bbox_half_width]))
+            pred_bbox_top= int(np.amax([0, prediction[0]-bbox_half_height]))
+            pred_bbox_bottom = int(np.amin([img_size[1], prediction[0]+bbox_half_height]))
 
-               # If no person is found in the frame
-               else:
-                   # TODO - I'm not sure that feeding the prediction into the update is correct but *shrug*
-                   prediction = np.dot(H,  kf.predict())[0]
-                   kf.update(prediction)
-                   print('Predicted center below')
-                   print(prediction)
-                   print('done with prediction')
+            cv2.rectangle(image, (pred_bbox_left, pred_bbox_top), (pred_bbox_right, pred_bbox_bottom), class_colors['prediction'], 1)
+            cv2.putText(image, 'prediction',
+                        (pred_bbox_left, pred_bbox_top - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                        class_colors['prediction'], 2)
 
-           # Use prediction
-           # TODO - how many number of frames is sustainable to keep using prediction?
-           # if not person_found:
+            cv2.imshow('image',image)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+
+            # If the person isn't found, the prediction is fed as a measurement
+            # TODO - This doesn't seem to be working as expected?
+            if not person_found:
+                # new_center = prediction+(prediction-prev_center)
+                # kf.update(new_center)
+                kf.update(prediction)
 
 
 if __name__ == "__main__":
-    # unconmment next line for an example of batch processing
-    # batch_detection_example()
     main()
--- kalmanfilter.py
+++ kalmanfilter.py
@@ -32,30 +32,30 @@ class KalmanFilter(object):
         self.x = self.x + np.dot(K, y)
         I = np.eye(self.n)
         self.P = np.dot(np.dot(I - np.dot(K, self.H), self.P),
-        	(I - np.dot(K, self.H)).T) + np.dot(np.dot(K, self.R), K.T)
+            (I - np.dot(K, self.H)).T) + np.dot(np.dot(K, self.R), K.T)
 
 def example():
-	dt = 1.0/60
-	F = np.array([[1, dt, 0], [0, 1, dt], [0, 0, 1]])
-	H = np.array([1, 0, 0]).reshape(1, 3)
-	Q = np.array([[0.05, 0.05, 0.0], [0.05, 0.05, 0.0], [0.0, 0.0, 0.0]])
-	R = np.array([0.5]).reshape(1, 1)
+    dt = 1.0/60
+    F = np.array([[1, dt, 0], [0, 1, dt], [0, 0, 1]])
+    H = np.array([1, 0, 0]).reshape(1, 3)
+    Q = np.array([[0.05, 0.05, 0.0], [0.05, 0.05, 0.0], [0.0, 0.0, 0.0]])
+    R = np.array([0.5]).reshape(1, 1)
 
-	x = np.linspace(-10, 10, 100)
-	measurements = - (x**2 + 2*x - 2)  + np.random.normal(0, 2, 100)
+    x = np.linspace(-10, 10, 100)
+    measurements = - (x**2 + 2*x - 2)  + np.random.normal(0, 2, 100)
 
-	kf = KalmanFilter(F = F, H = H, Q = Q, R = R)
-	predictions = []
+    kf = KalmanFilter(F = F, H = H, Q = Q, R = R)
+    predictions = []
 
-	for z in measurements:
-		predictions.append(np.dot(H,  kf.predict())[0])
-		kf.update(z)
+    for z in measurements:
+        predictions.append(np.dot(H,  kf.predict())[0])
+        kf.update(z)
 
-	import matplotlib.pyplot as plt
-	plt.plot(range(len(measurements)), measurements, label = 'Measurements')
-	plt.plot(range(len(predictions)), np.array(predictions), label = 'Kalman Filter Prediction')
-	plt.legend()
-	plt.show()
+    import matplotlib.pyplot as plt
+    plt.plot(range(len(measurements)), measurements, label = 'Measurements')
+    plt.plot(range(len(predictions)), np.array(predictions), label = 'Kalman Filter Prediction')
+    plt.legend()
+    plt.show()
 
 if __name__ == '__main__':
     example()
